76f9f71c669fd08fe6044d6132fe26a60de99cd5
prem-12345 <premkpatil1997@gmail.com>
2021-11-26 23:09:26 +0530

    [PREM]Add: Added icons component methods

--- src/app/component/icons/icons.component.ts
+++ src/app/component/icons/icons.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NotesService } from 'src/app/services/notesService/notes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { TrashNoteComponent } from '../trash-note/trash-note.component';
+import { ArchiveNotesComponent } from '../archive-notes/archive-notes.component';
+
 
 @Component({
   selector: 'app-icons',
@@ -7,9 +13,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IconsComponent implements OnInit {
 
-  constructor() { }
+  isTrashComponent:boolean = false;
+  isArchive:boolean = false;
+
+  @Input() noteCard: any;
+  
+  @Output() iconToDisplay = new EventEmitter<string>();
+
+
+  constructor(private notesService: NotesService, private _snackBar: MatSnackBar, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    let com = this.activatedRoute.snapshot.component;
+
+    if (com == TrashNoteComponent) {
+      this.isTrashComponent = true;
+      console.log(this.isTrashComponent);
+    }
+
+    if (com == ArchiveNotesComponent) {
+      this.isArchive = true;
+      console.log(this.isArchive);
+    }
+
   }
 
-}
+
+  // Delete note and move to trash
+  deleteNote() {
+    console.log(this.noteCard);
+
+    let deleteData = {
+      noteIdList: [this.noteCard.id],
+      isDeleted: true
+    }
+    console.log("deleted data", deleteData);
+
+    this.notesService.moveToTrash(deleteData).subscribe((response: any) => {
+      console.log(response);
+      this.iconToDisplay.emit(response);
+      this._snackBar.open('Note deleted Successful', '', {
+        duration: 2000,
+      })
+    }, error => {
+      console.log(error);
+      this._snackBar.open('Note delete Unsuccessful', '', {
+        duration: 2000,
+      })
+    }
+    )
+  }
+  
+
+  //Notes move to archive
+  moveToArchive(){
+    let req = {
+      noteIdList: [this.noteCard.id],
+      isArchived: true,
+    }
+    this.notesService.archivedservice(req).subscribe((response:any)=>{
+      console.log('move to archive');
+      this.iconToDisplay.emit(response)
+      console.log((response.data));
+    })
+  }
+
+
+  //Notes delete forever
+  deleteForever() {
+    
+    console.log(this.noteCard);
+
+    let deleteData = {
+      noteIdList: [this.noteCard.id],
+      isDeleted: false
+    }
+    console.log("deleted data", deleteData);
+
+    this.notesService.deleteNoteForever(deleteData).subscribe((response: any) => {
+      console.log(response);
+      this.iconToDisplay.emit(response)
+      this._snackBar.open('Note deleted Successful', '', {
+        duration: 2000,
+      })
+    }, error => {
+      console.log(error);
+      this._snackBar.open('Note delete Unsuccessful', '', {
+        duration: 2000,
+      })
+    }
+    )
+  }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
