d5102c15c1bac36567bf104efdb7d89df3543b24
prem-12345 <premkpatil1997@gmail.com>
2021-12-01 18:24:09 +0530

    [PREM] Add: Modify display note component

--- src/app/component/display-notes/display-notes.component.ts
+++ src/app/component/display-notes/display-notes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
 import { UpdateNoteComponent } from '../update-note/update-note.component';
 import {MatDialog} from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { TrashNoteComponent } from '../trash-note/trash-note.component';
 
 
 @Component({
@@ -10,24 +12,30 @@ import {MatDialog} from '@angular/material/dialog';
 })
 export class DisplayNotesComponent implements OnInit {
 
+  showPin:boolean=true;
   message:any;
 
   @Input() allNotes: any = [];
-
+  
+  
   @Output() displaytogetallnotes = new EventEmitter<string>();
  
-
-
   cards:any;
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog,private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
 
+    let com = this.activatedRoute.snapshot.component;
+
+    if(com == TrashNoteComponent){
+      this.showPin = false;
+      console.log(this.showPin);
+    }
   }
 
   openDialog(note:any) {
     const dialogRef = this.dialog.open(UpdateNoteComponent, 
-    { width:"370px",
+    { width:"600px",
       data:note
     });
 
