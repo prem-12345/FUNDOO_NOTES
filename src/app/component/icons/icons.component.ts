219d5d0ce45f18cda534f8e18a025ead35310876
prem-12345 <premkpatil1997@gmail.com>
2021-12-01 18:26:37 +0530

    [PREM] Add: update icons component ts

--- src/app/component/icons/icons.component.ts
+++ src/app/component/icons/icons.component.ts
@@ -4,6 +4,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { TrashNoteComponent } from '../trash-note/trash-note.component';
 import { ArchiveNotesComponent } from '../archive-notes/archive-notes.component';
+import { CollaboratorComponent } from '../collaborator/collaborator.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 
 @Component({
@@ -20,8 +23,10 @@ export class IconsComponent implements OnInit {
   
   @Output() iconToDisplay = new EventEmitter<string>();
 
+  colorarray = ['#F28B82', '#FBBC05', '#FFF475', '#CCFF90', '#A7FFEB','#CBF0F8','#AECBFA','#D7AEFB','#FDCFE8','#E6C9A8','#E8EAED',];
 
-  constructor(private notesService: NotesService, private _snackBar: MatSnackBar, private activatedRoute: ActivatedRoute) { }
+  
+  constructor(private notesService: NotesService, private _snackBar: MatSnackBar, private activatedRoute: ActivatedRoute, public dialog: MatDialog) { }
 
   ngOnInit(): void {
 
@@ -76,6 +81,26 @@ export class IconsComponent implements OnInit {
       console.log('move to archive');
       this.iconToDisplay.emit(response)
       console.log((response.data));
+      this._snackBar.open('Note archive Successful', '', {
+        duration: 2000,
+      })
+    })
+  }
+
+
+  // Unarchive notes
+  unArchive(){
+    let req = {
+      noteIdList: [this.noteCard.id],
+      isArchived: false,
+    }
+    this.notesService.archivedservice(req).subscribe((response:any)=>{
+      console.log('move to archive');
+      this.iconToDisplay.emit(response)
+      console.log((response.data));
+      this._snackBar.open('Note archive Successful', '', {
+        duration: 2000,
+      })
     })
   }
 
@@ -94,7 +119,7 @@ export class IconsComponent implements OnInit {
     this.notesService.deleteNoteForever(deleteData).subscribe((response: any) => {
       console.log(response);
       this.iconToDisplay.emit(response)
-      this._snackBar.open('Note deleted Successful', '', {
+      this._snackBar.open('Note deleted forever Successful', '', {
         duration: 2000,
       })
     }, error => {
@@ -106,7 +131,63 @@ export class IconsComponent implements OnInit {
     )
   }
 
+
+// restore notes
+  restore() {
+    console.log(this.noteCard);
+
+    let restoreData = {
+      noteIdList: [this.noteCard.id],
+      isDeleted: false
+    }
+    console.log("deleted data", restoreData);
+
+    this.notesService.moveToTrash(restoreData).subscribe((response: any) => {
+      console.log(response);
+      this.iconToDisplay.emit(response);
+      this._snackBar.open('Note restore Successful', '', {
+        duration: 2000,
+      })
+    }, error => {
+      console.log(error);
+      this._snackBar.open('Note restore Unsuccessful', '', {
+        duration: 2000,
+      })
+    }
+    )
   }
+
+  //change colour of cards
+  changecolor(data: any) {
+    let req = {
+      noteIdList: [this.noteCard.id],
+      color: data
+    }
+    
+    this.notesService.changecolorservice(req).subscribe((response: any) => {
+      console.log(response.data);
+      this.iconToDisplay.emit(response);
+
+    })
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(CollaboratorComponent, 
+    {width:'600px'
+    });
+
+    dialogRef.afterClosed().subscribe((result:any)=> {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+
+
+
+}
+
+
+  
 
 
 
